451abbfe6f7c2804f0a5f8fce49f5da823c18e09
David-0717 <ot18005@ues.edu.sv>
2024-11-06 00:25:09 -0600

    Agregando libro mayor y transacciones

---
+++ libromayor/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class CatalogoCuenta(models.Model):
+    idCuenta = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=5)
+    tipoDeCuenta = models.CharField(max_length=50)
+    nombreDeCuenta = models.CharField(max_length=100)
+    debe = models.FloatField(null=True, blank=True)
+    haber = models.FloatField(null=True, blank=True)
