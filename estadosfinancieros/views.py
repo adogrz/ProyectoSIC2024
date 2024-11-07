2d1458e02a8e670f42aaa9593222a49bc3ead17f
David-0717 <ot18005@ues.edu.sv>
2024-11-07 10:20:23 -0600

    agregando plantilla de balance y guardando saldos de cuentas

--- estadosfinancieros/views.py
+++ estadosfinancieros/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from libromayor.models import CatalogoCuenta
 
-# Create your views here.
+def balance_general(request):
+    # Obtener todas las cuentas de tipo Activo y Pasivo, y la cuenta con nombre "Capital Social"
+    catalogo_cuentas = CatalogoCuenta.objects.filter(tipoDeCuenta__in=["Activo", "Pasivo"])
+
+    total_debe = 0
+    total_haber = 0
+
+    # Calcular el total de debe y haber solo para las cuentas seleccionadas
+    for cuenta in catalogo_cuentas:
+        # Asegúrate de que los valores de 'debe' y 'haber' no sean nulos
+        cuenta.debe = cuenta.debe if cuenta.debe is not None else 0
+        cuenta.haber = cuenta.haber if cuenta.haber is not None else 0
+
+        # Sumar los valores a los totales
+        total_debe += cuenta.debe
+        total_haber += cuenta.haber
+
+    context = {
+        'catalogo_cuentas': catalogo_cuentas,  # Pasar las cuentas filtradas al contexto
+        'total_debe': total_debe,
+        'total_haber': total_haber,
+    }
+
+    return render(request, 'Balance_general.html', context)
